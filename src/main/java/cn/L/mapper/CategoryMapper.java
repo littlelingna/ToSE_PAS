009555181a0a17c964690d7931ab641450077d25
littlelingna <kln54321@163.com>
2018-10-22 20:28:27 +0800

    init

---
+++ src/main/java/cn/L/mapper/CategoryMapper.java
@@ -0,0 +1,25 @@
+package cn.L.mapper;
+ 
+import java.util.List;
+
+import cn.L.pojo.Category;
+import cn.L.util.Page;
+ 
+public interface CategoryMapper {
+ 
+      
+    public int add(Category category);  
+       
+      
+    public void delete(int id);  
+       
+      
+    public Category get(int id);  
+     
+      
+    public int update(Category category);   
+       
+      
+    public List<Category> list();
+      
+}
---
+++ src/main/java/cn/L/service/CategoryService.java
@@ -0,0 +1,11 @@
+package cn.L.service;
+
+import java.util.List;
+
+import cn.L.pojo.Category;
+import cn.L.util.Page;
+
+public interface CategoryService {
+
+	List<Category> list();
+}
